8e8a6f1db5386bdd07aaba0a46507549b8b355c0
manihatv <viswanathan.bojan@gmail.com>
2016-10-23 18:22:56 -0700

    Updating Hashing vulnerability mitigation changes_ver1.0

--- src/webapp/Hash.php
+++ src/webapp/Hash.php
@@ -7,19 +7,20 @@ use Symfony\Component\Config\Definition\Exception\Exception;
 class Hash
 {
 
-    //Begin - Change for vulnerability mitigation-Viswa
-    //static $salt = "password";
-	$salt = mcrypt_create_iv(16,MCRYPT_DEV_URANDOM);
+    	//Begin - Change for vulnerability mitigation-Viswa
+    	//static $salt = "password"; -- commented
+	static $salt;
 	//End - Change for vulnerability mitigation-Viswa
 
     public function __construct()
     {
+	   $salt = uniqid(mt_rand(), true); 
     }
 
     public static function make($plaintext)
     {
         //Begin - Change for vulnerability mitigation-Viswa
-        //return hash('sha1', $plaintext . Hash::$salt);
+        //return hash('sha1', $plaintext . Hash::$salt); --commented
         return hash('sha512', $plaintext . Hash::$salt);
         //End - Change for vulnerability mitigation-Viswa
 
