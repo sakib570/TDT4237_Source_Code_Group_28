c4bc7bf6605586dc0626bf53af90767090740322
manihatv <viswanathan.bojan@gmail.com>
2016-10-27 23:03:10 +0200

    Updating Hashing vulnerability mitigation changes_ver1.0
    Changing the code to use PASSWORD_BCRYPT for generating password hashes.

--- src/webapp/Hash.php
+++ src/webapp/Hash.php
@@ -9,26 +9,27 @@ class Hash
 
     	//Begin - Change for vulnerability mitigation-Viswa
     	//static $salt = "password"; -- commented
-	static $salt;
+	//static $salt; -- commented
 	//End - Change for vulnerability mitigation-Viswa
 
     public function __construct()
     {
-	   $salt = uniqid(mt_rand(), true); 
+	   //$salt = uniqid(mt_rand(), true); -- commented
     }
 
     public static function make($plaintext)
     {
         //Begin - Change for vulnerability mitigation-Viswa
         //return hash('sha1', $plaintext . Hash::$salt); --commented
-        return hash('sha512', $plaintext . Hash::$salt);
+        //return hash('sha512', $plaintext . Hash::$salt);-- commented
+	return password_hash($plaintext, PASSWORD_BCRYPT);    
         //End - Change for vulnerability mitigation-Viswa
 
     }
 
     public function check($plaintext, $hash)
     {
-        return $this->make($plaintext) === $hash;
+        //return $this->make($plaintext) === $hash;-- commented
+	return password_verify($plaintext, $hash);    
     }
-
 }
